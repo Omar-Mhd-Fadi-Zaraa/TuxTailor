60baaa922432b997b90097ea6b43591e170e8c21
Omar Zarraa <o.zarraa.2005@gmail.com>
2026-06-25 23:45:37 +0300

    Re-ordering, commenting out currently unused code, and added date_sent field

--- Application/TuxBackend/models/schemas.py
+++ Application/TuxBackend/models/schemas.py
@@ -1,43 +1,46 @@
-from pydantic import BaseModel, Field, model_validator
-from langchain_core.messages import HumanMessage, AIMessage, ToolMessage, SystemMessage, AnyMessage
-from typing import List
 from enum import Enum
 
-class Role(str,Enum):
+from pydantic import BaseModel, ConfigDict, Field
+from langchain_core.messages import HumanMessage
+
+
+class Role(str, Enum):
     USER: str = "user"
     ASSISTANT: str = "assistant"
     SYSTEM: str = "system"
     TOOL: str = "tool"
 
-class ChatMessage(BaseModel):
-    role: Role
-    content: str
-    tool_call_id: str | None = Field(default=None, alias="toolCallId")
-    tool_name: str | None = Field(default=None, alias="toolName")
 
-    model_config = {"populate_by_name": True}
+class ChatAgentRequest(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
 
-class ChatAgentHandling(BaseModel):
-    conversation_id: str = Field(alias="conversationId")
-    client_message_id: str = Field(alias="clientMessageId")
-    user_id: str | None = Field(default=None, alias="userId")
-
-
-class ChatAgentResponse(ChatAgentHandling):
-    messages: list[ChatMessage]
-
-    model_config = {"populate_by_name": True}
-
-    @model_validator(mode="after")
-    def last_message_must_be_assistant(self):
-        if not self.messages or self.messages[-1].role != Role.SYSTEM:
-            raise ValueError("messages[-1] must be a user message")
-        return self
-
-    
-class ChatAgentRequest(ChatAgentHandling):
+    user_id: int| None = Field(default=None, alias="userId")
+    chat_id: int = Field(alias="chatId")
     user_message: str = Field(alias="userMessage")
-    
+    date_sent: str = Field(alias="dateSent")
+
     def to_langchain_msg(self):
-        self.user_message = HumanMessage(content=self.user_message)
-        return self.user_message
+        return HumanMessage(content=self.user_message)
+
+
+
+# class ChatMessage(BaseModel):
+#     role: Role
+#     content: str
+#     tool_call_id: str | None = Field(default=None, alias="toolCallId")
+#     tool_name: str | None = Field(default=None, alias="toolName")
+
+#     model_config = {"populate_by_name": True}
+#     
+# class ChatAgentResponse(ChatAgentHandling):
+#     messages: list[ChatMessage]
+
+#     model_config = {"populate_by_name": True}
+
+#     @model_validator(mode="after")
+#     def last_message_must_be_assistant(self):
+#         if not self.messages or self.messages[-1].role != Role.SYSTEM:
+#             raise ValueError("messages[-1] must be a user message")
+#         return self
+
+
