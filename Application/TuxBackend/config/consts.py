b512cb58ba4e2f20b5f942ba366b6e433f5d830b
Omar Zarraa <o.zarraa.2005@gmail.com>
2026-06-27 01:33:59 +0300

    Added JWT consts

--- Application/TuxBackend/config/consts.py
+++ Application/TuxBackend/config/consts.py
@@ -4,3 +4,6 @@ from os import getenv
 load_dotenv()
 CHAT_MODEL = getenv("CHAT_MODEL")
 OLLAMA_BASE_URL = getenv("OLLAMA_BASE_URL")
+JWT_SECRET_KEY = getenv("JWT_SECRET_KEY")
+JWT_ALGORITHM = getenv("JWT_ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
