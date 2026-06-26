63f41a2fd96dbc46e646f724b05e56d98baf42af
Omar Zarraa <o.zarraa.2005@gmail.com>
2026-06-27 01:33:50 +0300

    Added the user router

--- Application/TuxBackend/app/main.py
+++ Application/TuxBackend/app/main.py
@@ -1,17 +1,16 @@
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
 
-from routes.routes import base, chat, lifespan
+from routes.routes import base, chat, user, lifespan
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(base)
 app.include_router(chat)
+app.include_router(user)
 
 
 @app.get("/scalar", include_in_schema=False)
 async def scalar_html():
     return get_scalar_api_reference(
-        openapi_url=app.openapi_url,
-        title=app.title,
-        dark_mode=True
+        openapi_url=app.openapi_url, title=app.title, dark_mode=True
     )
