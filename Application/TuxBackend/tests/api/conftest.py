0163c18ffd84d0afb80fc0e2494a18f114f5f7fd
Omar Zarraa <o.zarraa.2005@gmail.com>
2026-08-26 23:03:35 +0300

    Added a mocker to mock the validate_ollama_url function so that I don't need to have Ollama on for these tests

--- Application/TuxBackend/tests/api/conftest.py
+++ Application/TuxBackend/tests/api/conftest.py
@@ -1,9 +1,15 @@
 from fastapi.testclient import TestClient
 from app import main
+from unittest.mock import AsyncMock, MagicMock, patch
 import pytest
 
 
 @pytest.fixture(scope="module")
 def test_client():
-    with TestClient(main.app) as client:
+    mock_response = MagicMock()
+    mock_response.raise_for_status = lambda: None
+
+    with patch("routes.routes.validate_ollama_url", return_value=None), patch(
+        "httpx.AsyncClient.post", new_callable=AsyncMock, return_value=mock_response
+    ), TestClient(main.app) as client:
         yield client
